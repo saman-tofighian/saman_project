ca3e09314f511a6c1ab44595667ef1f675d2f551
saman <96416020+samanhtcs@users.noreply.github.com>
2022-12-15 12:02:41 +0330

    Add files via upload

---
+++ bootstrap-project2/index.js
@@ -0,0 +1,11 @@
+function saman1(){
+    document.getElementsByClassName('mobile')[0].classList.toggle('mobile2')
+}
+
+function saman2(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Thanks for Subscribing',
+        text: '♥'
+      })
+}
